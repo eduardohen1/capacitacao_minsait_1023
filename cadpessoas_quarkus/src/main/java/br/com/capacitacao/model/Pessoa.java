1cb76608e9984515d86df6c68be62af231671def
eherique_indra <eherique_indra@minsait.com>
2023-10-07 15:37:48 -0300

    App quarkus ok!

--- cadpessoas_quarkus/src/main/java/br/com/capacitacao/model/Pessoa.java
+++ cadpessoas_quarkus/src/main/java/br/com/capacitacao/model/Pessoa.java
@@ -1,2 +1,84 @@
-package br.com.capacitacao.model;public class Pessoa {
+package br.com.capacitacao.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class Pessoa extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome", length = 50)
+    private String nome;
+    @Column(name = "endereco", length = 100)
+    private String endereco;
+    @Column(name = "email", length = 200)
+    private String email;
+    @Column(name = "contato", length = 20)
+    private String contato;
+
+    public Pessoa() {}
+
+    public Pessoa(Long id, String nome, String endereco, String email, String contato) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.email = email;
+        this.contato = contato;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return Objects.equals(id, pessoa.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- cadpessoas_quarkus/src/main/java/br/com/capacitacao/resource/PessoaResource.java
+++ cadpessoas_quarkus/src/main/java/br/com/capacitacao/resource/PessoaResource.java
@@ -1,2 +1,65 @@
-package br.com.capacitacao.resource;public class PessoaResource {
+package br.com.capacitacao.resource;
+
+import br.com.capacitacao.model.Pessoa;
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.NotNull;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
+
+@Path("/pessoas")
+public class PessoaResource {
+
+    @POST
+    @Transactional
+    public Response addPessoa(Pessoa pessoa){
+        Pessoa newPessoa = new Pessoa();
+        newPessoa.setNome(pessoa.getNome());
+        newPessoa.setEndereco(pessoa.getEndereco());
+        newPessoa.setEmail(pessoa.getEmail());
+        newPessoa.setContato(pessoa.getContato());
+        newPessoa.persist();
+        return Response.ok(newPessoa).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Transactional
+    public Response getPessoa(@PathParam("id") Long id){
+        Pessoa pessoa = Pessoa.findById(id);
+        if(pessoa == null)
+            return Response.status(Response.Status.NOT_FOUND).build();
+        return Response.ok(pessoa).build();
+    }
+
+    @PUT
+    @Transactional
+    public Response updatePessoa(@NotNull Pessoa pessoa){
+        Pessoa upPessoa = Pessoa.findById(pessoa.getId());
+        if(upPessoa == null)
+            return Response.status(Response.Status.NOT_FOUND).build();
+        upPessoa.setNome(pessoa.getNome());
+        upPessoa.setEndereco(pessoa.getEndereco());
+        upPessoa.setEmail(pessoa.getEmail());
+        upPessoa.setContato(pessoa.getContato());
+        upPessoa.persist();
+        return Response.ok(upPessoa).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response deletePessoa(@PathParam("id") Long id){
+        Pessoa pessoa = Pessoa.findById(id);
+        if(pessoa == null)
+            return Response.status(Response.Status.NOT_FOUND).build();
+        pessoa.delete();
+        return Response.ok().build();
+    }
+
+    @GET
+    @Transactional
+    public Response listAllPessoas(){
+        return Response.ok(Pessoa.listAll()).build();
+    }
+
 }
